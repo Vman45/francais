b7e23d905748fc7e1bf9df83c06bd0392897c573
fingoldin <vassilioskaxiras@gmail.com>
2016-12-24 21:43:11 -0500

    first commit

---
+++ verbs.js
@@ -0,0 +1,39 @@
+VERBS =
+[
+	{
+		cname: "Les verbes irreguliers",
+		verbs:
+		[
+			{
+				inf: "Avoir",
+				tran: "to have",
+				je: "ai",
+				tu: "as",
+				il: "a",
+				nous: "avons",
+				vous: "avez",
+				ils: "ont"
+			},
+			{
+				inf: "&ecirc;tre",
+	                	tran: "to be",
+	                	je: "suis",
+	                	tu: "es",
+	                	il: "est",
+	                	nous: "sommes",
+	                	vous: "&ecirc;tes",
+	        	        ils: "sont"
+			},
+			{
+		                inf: "Aller",
+                		tran: "to go",
+                		je: "vais",
+                		tu: "vas",
+                		il: "va",
+                		nous: "allons",
+                		vous: "allez",
+                		ils: "vont"
+        		}
+		]
+	}
+];
